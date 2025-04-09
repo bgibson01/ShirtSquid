4f1103f9f36e3ff2bb5a281f97f06ac55c8a9993
sploozie <bgibson01@gmail.com>
2025-04-09 16:29:07 -0400

    1

--- js/gallery-data.js
+++ js/gallery-data.js
@@ -3,19 +3,26 @@ const pageId = "880201272040785"; // Replace with your Facebook page ID
 
 const galleryData = [];
 
-fetch(`https://graph.facebook.com/v12.0/${pageId}/posts?fields=message,full_picture&access_token=${accessToken}`)
-  .then(response => response.json())
-  .then(data => {
-    data.data
-      .filter(post => post.full_picture) // Only include posts with images
-      .forEach((post, index) => {
-        galleryData.push({
-          id: index + 1,
-          src: post.full_picture,
-          caption: post.message || "No caption available"
-        });
+// Create a Promise to notify when data is ready
+const fetchGalleryData = new Promise((resolve, reject) => {
+    fetch(`https://graph.facebook.com/v12.0/${pageId}/posts?fields=message,full_picture&access_token=${accessToken}`)
+      .then(response => response.json())
+      .then(data => {
+        data.data
+          .filter(post => post.full_picture) // Only include posts with images
+          .forEach((post, index) => {
+            galleryData.push({
+              id: index + 1,
+              src: post.full_picture,
+              caption: post.message || "No caption available"
+            });
+          });
+  
+        console.log("Gallery data fetched:", galleryData); // Check the populated array in the console
+        resolve(); // Notify that the data is ready
+      })
+      .catch(error => {
+        console.error("Error fetching Facebook posts:", error);
+        reject(error); // Notify that an error occurred
       });
-
-    console.log(galleryData); // Check the populated array in the console
-  })
-  .catch(error => console.error("Error fetching Facebook posts:", error));
+  });
--- js/gallery.js
+++ js/gallery.js
@@ -7,55 +7,64 @@ document.addEventListener("DOMContentLoaded", () => {
     return;
   }
 
-  console.log("Gallery container found. Rendering items...");
+  console.log("Waiting for gallery data...");
 
-  galleryData.forEach((item) => {
-    console.log(`Processing item: ${item.src}`);
+  // Wait for the gallery data to be fetched
+  fetchGalleryData
+    .then(() => {
+      console.log("Gallery data ready. Rendering items...");
 
-    // Add gallery item
-    const galleryItem = `
-      <div class="col-sm-4 portfolio-item">
-        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${item.id}">
-          <div class="caption">
-            <div class="caption-content">
-              <i class="fa fa-search-plus fa-3x"></i>
-            </div>
+      galleryData.forEach((item) => {
+        console.log(`Processing item: ${item.src}`);
+
+        // Add gallery item
+        const galleryItem = `
+          <div class="col-sm-4 portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${item.id}">
+              <div class="caption">
+                <div class="caption-content">
+                  <i class="fa fa-search-plus fa-3x"></i>
+                </div>
+              </div>
+              <img alt="${item.caption}" class="img-responsive" src="${item.src}">
+            </a>
           </div>
-          <img alt="${item.caption}" class="img-responsive" src="${item.src}">
-        </a>
-      </div>
-    `;
-    galleryContainer.insertAdjacentHTML("beforeend", galleryItem);
-    console.log(`Gallery item added: ${item.src}`);
+        `;
+        galleryContainer.insertAdjacentHTML("beforeend", galleryItem);
+        console.log(`Gallery item added: ${item.src}`);
 
-    // Add modal
-    const modalItem = `
-      <div aria-hidden="true" class="portfolio-modal modal fade" id="portfolioModal${item.id}" role="dialog" tabindex="-1">
-        <div class="modal-content">
-          <div class="close-modal" data-dismiss="modal">
-            <div class="lr">
-              <div class="rl"></div>
-            </div>
-          </div>
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8 col-lg-offset-2">
-                <div class="modal-body">
-                  <h2>Shirt Squid</h2>
-                  <hr class="star-primary">
-                  <img alt="${item.caption}" class="img-responsive img-centered" src="${item.src}">
-                  <p>${item.caption}</p>
-                  <button class="btn btn-default" data-dismiss="modal" type="button">
-                    <i class="fa fa-times"></i> Close
-                  </button>
+        // Add modal
+        const modalItem = `
+          <div aria-hidden="true" class="portfolio-modal modal fade" id="portfolioModal${item.id}" role="dialog" tabindex="-1">
+            <div class="modal-content">
+              <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                  <div class="rl"></div>
+                </div>
+              </div>
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-8 col-lg-offset-2">
+                    <div class="modal-body">
+                      <h2>Shirt Squid</h2>
+                      <hr class="star-primary">
+                      <img alt="${item.caption}" class="img-responsive img-centered" src="${item.src}">
+                      <p>${item.caption}</p>
+                      <button class="btn btn-default" data-dismiss="modal" type="button">
+                        <i class="fa fa-times"></i> Close
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    `;
-    modalContainer.insertAdjacentHTML("beforeend", modalItem);
-    console.log(`Modal added for item: ${item.src}`);
-  });
+        `;
+        modalContainer.insertAdjacentHTML("beforeend", modalItem);
+        console.log(`Modal added for item: ${item.src}`);
+      });
+    })
+    .catch((error) => {
+      console.error("Failed to render gallery:", error);
+    });
 });
